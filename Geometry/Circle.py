1c9a16f997cd18bda09c28386585621683c4efaf
Olamide Olatunji <conanupme@gmail.com>
2025-03-01 15:52:39 -0500

    Finshing For the Day, Testing Tommrow

--- Geometry/Circle.py
+++ Geometry/Circle.py
@@ -5,19 +5,38 @@ def findcirumfrence(inputvar):
     findinginputdef = True
     while findinginputdef == True:
         inputdef = input("Diameter/Circumfrence/Raduis/Area(D/C/R/A): ").strip().lower()
-        if inputdef == "D" or inputdef == "C" or inputdef=="R":
+        if inputdef == "d" or inputdef == "c" or inputdef=="r" or inputdef == "a":
             findinginputdef = False
             break
     if inputdef == "c":
         return "You Already have the Circumfrence Silly"
-    if input == "r":
+    if inputdef == "r":
         val = 2 * math.pi * inputvar
         return val,round(val), round(val,2)
-    if input == "d":
+    if inputdef == "d":
         r = inputvar/2
         val = 2 * math.pi*r
         return val,round(val), round(val,2)
-    if input == "A"
+    if inputdef == "a":
+        r = math.sqrt(inputvar/math.pi)
+        val = 2*math.pi*inputvar
+        return val,round(val), round(val,2)
+def findraduis(inputvar):
+    findinginputdef = True
+    while findinginputdef == True:
+        inputdef = input("Diameter/Circumfrence/Raduis/Area(D/C/R/A): ").strip().lower()
+        if inputdef == "d" or inputdef == "c" or inputdef=="r" or inputdef == "a":
+            findinginputdef = False
+            break
+        if inputdef == "r":
+            return "You already Have the raduis"
+        if inputdef == "d":
+            return inputvar/2, math.round(inputvar/2,2)
+        if inputdef == "a":
+            r = math.sqrt(inputvar/math.pi)
+            return r, round(r,2)
+        
+
 
 
 
